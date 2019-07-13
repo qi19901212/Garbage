38d8897bfea49ae688f6cf9530abc66a3f31e6ed
sun <qi19901212@aliyun.com>
2019-07-13 13:13:23 +0800

    update md

--- README.md
+++ README.md
@@ -5,10 +5,13 @@
 
 - 数据库：存储四种垃圾分类的相关垃圾数据， 数据分别为sort，product。把sort.json 和product.csv 导入云数据库即可
 - 云函数：获取百度识别库的accessToken
+[百度AI识别库地址](http://ai.baidu.com/docs#/ImageClassify-API/ebc492b1)
+[QQ AI地址](https://ai.qq.com/)
 
 ## 需要修改为自己的key 
 1. 小程序key 在文件project.config.json->appid 记住创建小程序的时候选择云开发
 2. 百度key 主要做拍照识别的cloudfunctions->baiduAccessToken->index->clientId和clientSecret
+此处替换为：API Key 和 Secret Key
 3. 智能询问采用ai.qq 的智能闲聊接口 key在pages->android->qa->app_id 还需要app-key pages->utils->util->signTengxunAI的app_key
 
 ## 直接扫码体验
--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,7 @@
 //app.js
 App({
-  onLaunch: function () {
-    
+  onLaunch: function() {
+
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -9,8 +9,27 @@ App({
         traceUser: true,
       })
     }
+    const updateManager = wx.getUpdateManager()
+    updateManager.onCheckForUpdate(function(res) {
+      console.log(res.hasUpdate)
+      if (res.hasUpdate) {
+        updateManager.onUpdateReady(function() {
+          wx.showModal({
+            title: '更新提示',
+            content: '新版本已经准备好，是否重启应用？',
+            success: function(res) {
+              if (res.confirm) {
+                updateManager.applyUpdate()
+              }
+            }
+          })
+        })
+      }
+    })
+    updateManager.onUpdateFailed(function() {
+      // 新版本下载失败
+    })
 
   },
-  globalData: {
-  }
+  globalData: {}
 })
